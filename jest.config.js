78a78b097aa7f303e8470166d8df9d04bf9669c0
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-01-27 17:17:01 +0000

    More config stuff

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['<rootDir>/**/*.ts'],
+  testMatch: ['<rootDir>/src/**/*.ts'],
 };
